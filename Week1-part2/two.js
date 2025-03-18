48b5d1f21fbbb2b13f570e4b4470b05bd36b4b16
vivsam <hi@coolkidz.me>
2025-03-18 10:06:28 +0200

    WIP: Implemented the week one's tasks part-2.

---
+++ Week1-part2/two.js
@@ -0,0 +1,11 @@
+/*
+<script>
+    let numbers = [];
+    while (numbers.length < 5) numbers.push(parseFloat(prompt('Enter number ' + (numbers.length + 1) + ': ')))
+
+</script>
+(hold shitty code, apparently I had to do it in pure javascript.)
+*/
+
+let numbers = [];
+while (numbers.length < 5) numbers.push(parseFloat(prompt('Enter number ' + (numbers.length + 1) + ': ')))
